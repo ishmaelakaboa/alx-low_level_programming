ec5c81831cbaa657f767f66ab103d60a7c24e365
mawusi25 <sirtemple25@gmail.com>
2022-03-21 08:32:30 +0000

    my c pointers array

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- * _strcpy - create a copy of a string
- * @dest: will contain the copy of the original string
- * @src: Contains the original string
- * Return: Gives back the copy of string
+ * _strncpy - Copies a string up to an n byte
+ * @dest: Destination string
+ * @src: the source of the string
+ * @n: number of bytes to look
+ * Return: Returns a pointer to the resulting string dest
  */
-char *_strcpy(char *dest, char *src)
+
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i]; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
